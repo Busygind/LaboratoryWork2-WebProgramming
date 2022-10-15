f74751cfdb69e4fd300b0f20bfa98da415249dcd
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-10-15 13:52:34 +0300

    add filter to AreaCheck

---
+++ src/main/java/busygind/filters/AreaCheckFilter.java
@@ -0,0 +1,29 @@
+package busygind.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebFilter(value = "/area-check-servlet")
+public class AreaCheckFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        if (request.getDispatcherType().equals(DispatcherType.REQUEST)) {
+            PrintWriter writer = res.getWriter();
+            writer.println("Direct access to AreaCheckServlet is forbidden!");
+            res.setStatus(403);
+            writer.close();
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+}
